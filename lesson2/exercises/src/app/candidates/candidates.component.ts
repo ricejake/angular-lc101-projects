d7ad39e1460bb364fbecef9b564a632c6242613c
ricejake <ecirbocaj@yahoo.com>
2021-04-04 12:54:40 -0500

    finished part 2

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidatesComponent implements OnInit {
    missionName = "LaunchCode Moonshot"
-
    candidates = [
     {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
     {name: 'Tessa Tortoise', data: {age: 126, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
@@ -21,10 +20,15 @@ export class CandidatesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
   // Code the addToCrew function here:
-
+   addToCrew = (candidate) => {
+    if (!this.crew.includes(currentCrewMember => currentCrewMember === candidate)){
+      this.crew.push(candidate);
+    }
+    }
 
   // BONUS: Code the changeMissionName function here:
 
